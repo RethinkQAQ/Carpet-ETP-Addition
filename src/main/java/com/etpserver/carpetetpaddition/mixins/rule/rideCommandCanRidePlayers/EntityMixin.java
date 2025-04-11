c65cb9df536b5c38b28b1ed4e21d6cdb00cacb20
Kathleen_Come <kathleen_come@outlook.com>
2025-04-11 22:51:04 +0800

    [fix] fix player be ride can see

--- src/main/java/com/etpserver/carpetetpaddition/mixins/rule/rideCommandCanRidePlayers/EntityMixin.java
+++ src/main/java/com/etpserver/carpetetpaddition/mixins/rule/rideCommandCanRidePlayers/EntityMixin.java
@@ -1,23 +1,34 @@
 package com.etpserver.carpetetpaddition.mixins.rule.rideCommandCanRidePlayers;
 
 import com.etpserver.carpetetpaddition.settings.CarpetETPSettings;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.entity.Entity;
+import net.minecraft.network.packet.s2c.play.EntityPassengersSetS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public class EntityMixin {
-    @WrapOperation(
-            method = "method_37217",
-            constant = @Constant(
-                    classValue = ServerPlayerEntity.class
-        )
+
+    @Shadow
+    @Nullable
+    private Entity vehicle;
+
+    @Inject(
+            method = "startRiding(Lnet/minecraft/entity/Entity;Z)Z",
+            at = @At(
+                    value = "RETURN"
+            )
     )
-    private static boolean startRiding(Object object, Operation<Boolean> original) {
-        return CarpetETPSettings.rideCommandCanRidePlayers || original.call(object);
+    private void sendRidePacket(Entity entity, boolean force, CallbackInfoReturnable<Boolean> cir) {
+        if (CarpetETPSettings.rideCommandCanRidePlayers && entity instanceof ServerPlayerEntity) {
+            if (this.vehicle != null && !this.vehicle.getWorld().isClient) {
+                ((ServerPlayerEntity) vehicle).networkHandler.sendPacket(new EntityPassengersSetS2CPacket(vehicle));
+            }
+        }
     }
 }
